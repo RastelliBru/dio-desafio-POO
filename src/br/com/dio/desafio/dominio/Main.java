556562356a1ccc92810a4599b064dbf9b11e8bde
RastelliBru <bruna.rastelli@gmail.com>
2022-07-24 22:38:39 -0300

    final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -19,8 +19,40 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devBruna = new Dev();
+        devBruna.setNome("Bruna");
+        devBruna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Bruna" + devBruna.getConteudosInscritos());
+        devBruna.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Bruna" + devBruna.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Bruna" + devBruna.getConteudosConcluidos());
+        System.out.println("XP:" + devBruna.calcularTotalXP());
+
+        System.out.println("--------------------");
+
+        Dev devKaue = new Dev();
+        devKaue.setNome("Kaue");
+        devKaue.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Kaue" + devKaue.getConteudosInscritos());
+        devKaue.progredir();
+        devKaue.progredir();
+        devKaue.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Kaue" + devKaue.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Kaue" + devKaue.getConteudosConcluidos());
+        System.out.println("XP:" + devKaue.calcularTotalXP());
+
     }
 }
